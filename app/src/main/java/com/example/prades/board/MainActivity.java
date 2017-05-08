18ee37740c933a008d7ed4e9c6a303d48358622a
SongYi Lee <qwe52165@gmail.com>
2017-05-09 00:25:58 +0900

    login_page complete

--- app/src/main/java/com/example/prades/board/MainActivity.java
+++ app/src/main/java/com/example/prades/board/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.prades.board;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText login_id = (EditText)findViewById(R.id.login_id);
+    EditText login_pw = (EditText)findViewById(R.id.login_password);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+
+    login_id.addTextChangedListener(){
+        
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if (keyCode==KeyEvent.KEYCODE_ENTER)
+        {
+            // Just ignore the [Enter] key
+            return true;
+        }
+        // Handle all other keys in the default way
+        return super.onKeyDown(keyCode, event);
     }
 }
