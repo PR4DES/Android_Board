d6c556f925d25242240b0e0e4bae9e1e7b127a2b
SongYi Lee <qwe52165@gmail.com>
2017-05-10 01:07:25 +0900

    searchpage layout

---
+++ app/src/main/java/com/example/prades/board/login.java
@@ -0,0 +1,33 @@
+package com.example.prades.board;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class login extends AppCompatActivity {
+
+    EditText login_id = (EditText)findViewById(R.id.login_id);
+    EditText login_pw = (EditText)findViewById(R.id.login_password);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        login_id.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+}
